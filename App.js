de59efb28c2432b5abbdb3c0a119b8d8af5b1a0d
abdulrehman <chrehman1998@gmail.com>
2020-11-27 11:06:28 +0500

    Task4 Completed

--- App.js
+++ App.js
@@ -9,38 +9,56 @@ export default function App() {
   const[history,setHistory]=useState([{save:"",originalPrice:""}])
 
   const changePrice=(text)=>{
+    if(text>=0){
       setPrice(text);
+    }else{
+      alert("Enter Price In Positive")
+      setPrice(0)
+    }
+      
   }
   const changeDiscount=(text)=>{
-    setDiscount(text)
+   
+      setDiscount(text)
+   
+    
     
   }
 
   const calculateDiscount=()=>{
+      if(discount>0 && discount<=100){
         var calculation=price*(discount*0.01)
-        setCalDiscount(calculation);
+        
+        setCalDiscount(calculation.toFixed(2));
+      } else{
+        alert("Enter Discount between 1-100");
+        setDiscount()
+      }
+        
   }
   return (
     <View style={styles.container}>
-      <Text style={{fontSize:50}}> Discount Calculator</Text> 
-      <TextInput style={{ marginTop: 10, fontSize: 30 }}
+      <Text style={{fontSize:30}}> Discount Calculator</Text> 
+      <View >
+      <TextInput style={{marginTop:20,marginBottom:20,padding:5,width:300,fontSize: 30 ,borderWidth:3}}
         placeholder="Enter Price"
         keyboardType='numeric'
         onChangeText={(text) => changePrice(text)}
         value={price}
       /> 
-      <TextInput style={{ marginTop: 10, fontSize: 30 }}
+      <TextInput style={{ marginBottom:20,padding:5,width:300, fontSize: 30,borderWidth:3 }}
       placeholder="Enter Discount"
         keyboardType='numeric'
         onChangeText={(text) => {
           changeDiscount(text)
           console.log(discount);
-          // calculateDiscount();
+         // calculateDiscount();
         }}
         value={discount}
       />
+      </View>
       <Button  onPress={calculateDiscount} title="Calculate"/>
-      <Text style={{fontSize:30}}>Result:</Text>
+      <Text style={{marginTop:10,fontSize:30}}>Result:</Text>
       <Text style={{fontSize:30}}>You Save:{calDiscount}</Text>
       <Text style={{fontSize:30}}>Original Price:{price}</Text>
   
@@ -50,7 +68,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    marginTop:25,
+    padding:10,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
