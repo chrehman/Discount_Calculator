fa7694d9877230d2ab567a9f1db987d7a0b185fd
abdulrehman <chrehman1998@gmail.com>
2020-11-27 09:52:30 +0500

    Adding TextInput Fields

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
 
 export default function App() {
+  const[price,setPrice]=useState()
+  const[discount,setDiscount]=useState()
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{fontSize:"50"}}> Discount Calculator</Text> 
+      <TextInput style={{ marginTop: 10, fontSize: 30 }}
+        placeholder="Enter Price"
+        keyboardType='numeric'
+        onChangeText={(text) => setPrice(text)}
+        value={price}
+      /> 
+      <TextInput style={{ marginTop: 10, fontSize: 30 }}
+      placeholder="Enter Discount"
+        keyboardType='numeric'
+        onChangeText={(text) => setDiscount(text)}
+        value={discount}
+      />
     </View>
   );
 }
